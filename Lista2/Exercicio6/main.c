ac4555a1a59429d4727f1dec65c8e91959b8c18a
Nara Tatsue Sakamoto <nara.sakamoto@gmail.com>
2018-10-14 18:50:02 -0300

    Exercicio6Lista2

---
+++ Lista2/Exercicio6/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+long long bonati(int n){
+  if(n==0 ) return 0;
+  else if(n==1) return 1;
+  else {
+    if(n%2==0) return bonati(n-1)+bonati(n-2);
+    else if(n%2!=0) return bonati(n-1)*bonati(n-2);
+  }
+  
+
+}
+int main(void) {
+  int num;
+  while(scanf("%d", &num)!=EOF){
+
+  printf("%lld\n", bonati(num-1));
+  }
+  return 0;
+}
